6214c3a2bfe391339f8b90fb503a6b3352df02f5
harry-z <shinji_akari@163.com>
2023-05-07 21:42:55 +0800

    WIP

--- Shaders/AO.hlsl
+++ Shaders/AO.hlsl
@@ -1,29 +1,59 @@
 #include "Shaders/VSHeader.h"
-#include "Shaders/PSHeader.h"
 
 struct VS_PopulateDepth
 {
     float4 HPosition            : SV_Position;
     float3 ViewPos              : TEXCOORD0;
+    float3 ViewNormal           : TEXCOORD1;
+    float3 ViewTangent          : TEXCOORD2;
 };
 
-VS_PopulateDepth OutputViewSpaceDepthVS(float3 InPosition : POSITION)
+VS_PopulateDepth OutputViewSpaceDepthVS(VS_In_PNTUV0 IN)
 {
     VS_PopulateDepth PopulateDepth;
-    PopulateDepth.HPosition = mul(float4(InPosition, 1.0f), WorldViewProj);
-    PopulateDepth.ViewPos = mul(float4(InPosition, 1.0f), WorldView);
+    PopulateDepth.HPosition = mul(float4(IN.Position, 1.0f), WorldViewProj);
+    PopulateDepth.ViewPos = mul(float4(IN.Position, 1.0f), WorldView);
+    PopulateDepth.ViewNormal = mul(float4(IN.Normal, 0.0f), WorldViewIT);
+    PopulateDepth.ViewTangent = mul(float4(IN.Tangent, 0.0f), WorldViewIT);
     return PopulateDepth;
 }
 
-float4 OutputViewSpaceDepthPS(VS_PopulateDepth InData) : SV_Target0
+struct PS_PopulateDepth
 {
-    return float4(InData.ViewPos, 0.0f);
+    float4 ViewPos              : SV_Target0;
+    float4 ViewNormal           : SV_Target1;
+    float4 ViewTangent          : SV_Target2;
+};
+
+PS_PopulateDepth OutputViewSpaceDepthPS(VS_PopulateDepth InData)
+{
+    PS_PopulateDepth PopulateDepthBuffer;
+    PopulateDepthBuffer.ViewPos = float4(InData.ViewPos, 0.0f);
+    PopulateDepthBuffer.ViewNormal = float4(InData.ViewNormal, 0.0f);
+    PopulateDepthBuffer.ViewTangent = float4(InData.ViewTangent, 0.0f);
+    return PopulateDepthBuffer;
 }
 
-Texture2D ViewPosMap : register(t0);
-SamplerState ViewPosMapSampler : register(s0);
-
-float4 HBAO(FullScreenQuadOutput InData) : SV_TARGET0
+cbuffer AOParams : register(b0)
 {
-    
+    float ViewSpaceSampleRadius;
+    float2 FocalLen;
+    float2 WindowParams;
+};
+
+Texture2D <float4> ViewPosMap                : register(t0);
+Texture2D <float4> ViewNormalMap             : register(t1);
+Texture2D <float4> ViewTangentMap            : register(t2);
+SamplerState TextureSampler                  : register(s0);
+
+float4 HBAO(float4 HPosition : SV_Position) : SV_Target0;
+{
+    float2 SampleRadius = 0.5f * ViewSpaceSampleRadius * FocalLen / HPosition.w;
+    float PixelRadius = SampleRadius.x * WindowParams.x;
+
+    float AOResult = 1.0f;
+    [branch] if (PixelRadius > 1.0f)
+    {
+
+    }
 }
--- Shaders/CommonHeader.h
+++ Shaders/CommonHeader.h
@@ -1,11 +1,35 @@
 #define PI 3.1415926f
 
-struct FullScreenQuadOutput
-{
-    float4 HPosition : SV_Position;
-};
-
 float4 FullScreenQuad(float2 InPosition : POSITION) : SV_Position
 {
     return float4(InPosition, 0.0f, 1.0f);
+}
+
+float RadicalInverse_VdC(uint bits) {
+    bits = (bits << 16u) | (bits >> 16u);
+    bits = ((bits & 0x55555555u) << 1u) | ((bits & 0xAAAAAAAAu) >> 1u);
+    bits = ((bits & 0x33333333u) << 2u) | ((bits & 0xCCCCCCCCu) >> 2u);
+    bits = ((bits & 0x0F0F0F0Fu) << 4u) | ((bits & 0xF0F0F0F0u) >> 4u);
+    bits = ((bits & 0x00FF00FFu) << 8u) | ((bits & 0xFF00FF00u) >> 8u);
+    return float(bits) * 2.3283064365386963e-10; // / 0x100000000
+}
+
+float2 Hammersley2d(uint i, uint N) {
+    return float2(float(i)/float(N), RadicalInverse_VdC(i));
+}
+
+float Random2DTo1D(float2 Value, float a, float b)
+{
+    float2 SmallValue = sin(Value);
+	float Random = dot(SmallValue, b);
+	Random = frac(sin(Random) * a);
+	return Random;
+}
+
+float2 Random2DTo2D(float2 Value)
+{
+    return float2(
+		Random2DTo1D(Value, 14375.5964f, float2(15.637f, 76.243f)),
+		Random2DTo1D(Value, 14684.6034f, float2(45.366f, 23.168f))
+	);
 }
